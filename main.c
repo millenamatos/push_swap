921a05b2c928d0339dfed7d68f1b62b3e618beb7
palenrick <paulo_henrique.st@hotmail.com>
2026-07-24 11:54:10 -0300

    test(main): add temporary driver to parse and print stack A

--- main.c
+++ main.c
@@ -6,16 +6,25 @@
 /*   By: pauhenr2 <pauhenr2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 18:43:57 by pauhenr2          #+#    #+#             */
-/*   Updated: 2026/07/23 21:17:02 by pauhenr2         ###   ########.fr       */
+/*   Updated: 2026/07/24 11:48:07 by pauhenr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
+	t_node	*stack_a;
+
 	if (argc < 2)
 		return (0);
-	write(1, &*argv[1], 1);
+	stack_a = NULL;
+	parse_args(argc, argv, &stack_a);
+	while (stack_a)
+	{
+		printf("%d\n", stack_a->value);
+		stack_a = stack_a->next;
+	}
 	return (0);
 }
