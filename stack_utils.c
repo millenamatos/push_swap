3c3a886a0b7b00a486e3c31bbdc6210471446b90
palenrick <paulo_henrique.st@hotmail.com>
2026-08-10 22:36:16 -0300

    refactor(stack): atualização de funções uteis para receber o struct t_stack e lidar com seu conteudo

--- stack_utils.c
+++ stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: pauhenr2 <pauhenr2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 23:47:27 by pauhenr2          #+#    #+#             */
-/*   Updated: 2026/08/10 19:12:23 by pauhenr2         ###   ########.fr       */
+/*   Updated: 2026/08/10 22:33:30 by pauhenr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,39 +28,45 @@ t_node	*create_node(int value)
 	if (!new_node)
 		return (NULL);
 	new_node->value = value;
+	new_node->index = -1;
 	new_node->next = NULL;
 	return (new_node);
 }
 
-void	stack_add_back(t_node **stack, t_node *new_node)
+void	stack_add_back(t_stack *stack, t_node *new_node)
 {
 	t_node	*last_node;
 
 	if (!stack || !new_node)
 		return ;
-	if (!*stack)
+	if (!stack->top)
+		stack->top = new_node;
+	else
 	{
-		*stack = new_node;
-		return ;
+		last_node = stack->top;
+		while (last_node->next)
+			last_node = last_node->next;
+		last_node->next = new_node;
 	}
-	last_node = *stack;
-	while (last_node->next)
-		last_node = last_node->next;
-	last_node->next = new_node;
+	stack-size++;
 }
 
-void	free_stack(t_node **stack)
+void	free_stack(t_stack *stack)
 {
+	t_node	*current;
 	t_node	*temp;
 
-	if (!stack || !*stack)
+	if (!stack || !stack->top)
 		return ;
-	while (*stack)
+	current = stack->top;
+	while (current)
 	{
-		temp = (*stack)->next;
-		free(*stack);
-		*stack = temp;
+		temp = current->next;
+		free(current);
+		current = temp;
 	}
+	stack->top = NULL;
+	size->size = 0;
 }
 
 void	push_to_stack(t_stack *stack, t_node *new_node)
